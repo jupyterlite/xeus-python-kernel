1b6f9ad6a3f9093ce6f12dcd4cadb30180f55456
martinRenou <martin.renou@gmail.com>
2022-12-21 15:20:30 +0100

    Update tests

--- tests/test_xeus_python_env.py
+++ tests/test_xeus_python_env.py
@@ -56,28 +56,3 @@ def test_python_env_from_file_1():
 
     os.remove(Path(addon.cwd.name) / "python_data.js")
     os.remove(Path(addon.cwd.name) / "python_data.data")
-
-
-def test_python_env_from_file_2():
-    app = LiteStatusApp(log_level="DEBUG")
-    app.initialize()
-    manager = app.lite_manager
-
-    addon = XeusPythonEnv(manager)
-    addon.environment_file = "environment-2.yml"
-
-    for step in addon.post_build(manager):
-        pass
-
-    # Check env
-    assert os.path.isdir("/tmp/xeus-python-kernel/envs/xeus-python-kernel-2")
-
-    assert os.path.isfile("/tmp/xeus-python-kernel/envs/xeus-python-kernel-2/bin/xpython_wasm.js")
-    assert os.path.isfile("/tmp/xeus-python-kernel/envs/xeus-python-kernel-2/bin/xpython_wasm.wasm")
-
-    # Check empack output
-    assert os.path.isfile(Path(addon.cwd.name) / "python_data.js")
-    assert os.path.isfile(Path(addon.cwd.name) / "python_data.data")
-
-    os.remove(Path(addon.cwd.name) / "python_data.js")
-    os.remove(Path(addon.cwd.name) / "python_data.data")
