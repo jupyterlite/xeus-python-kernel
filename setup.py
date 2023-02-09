d4da83a6627feb552535776e7d41d73b1c69afd4
martinRenou <martin.renou@gmail.com>
2023-02-09 10:34:00 +0100

    Bump empack

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup_args = dict(
         "traitlets",
         "jupyterlite",
         "requests",
-        "empack>=2.0.6,<3",
+        "empack>=2.0.7,<3",
         "typer",
     ],
     zip_safe=False,
