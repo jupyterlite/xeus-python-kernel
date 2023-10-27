76a41204c14868327943dcf3477705c1be2aa8ef
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-10-27 09:12:29 +0000

    Test importing packages

--- ui-tests/test/execute.test.ts
+++ ui-tests/test/execute.test.ts
@@ -23,4 +23,14 @@ test.describe('Code execution', () => {
     expect(output).toBeTruthy();
     expect(output![0]).toBe('4');
   });
+
+
+  test('Import critical packages', async ({ page }) => {
+    await page.notebook.createNew();
+    await page.notebook.setCell(0, 'code', 'import scipy\nscipy.__version__');
+    await page.notebook.run();
+    const output = await page.notebook.getCellTextOutput(0);
+
+    expect(output).toBeTruthy();
+  });
 });
